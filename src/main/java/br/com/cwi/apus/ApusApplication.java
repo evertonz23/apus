88a78046d64466a6559e9e3f96dcf4d7430a9ad3
Everton Zanatta <everton.zanatta.nh@gmail.com>
2021-07-15 19:43:57 -0300

    Exemplo de chamada externa

--- src/main/java/br/com/cwi/apus/ApusApplication.java
+++ src/main/java/br/com/cwi/apus/ApusApplication.java
@@ -2,6 +2,9 @@ package br.com.cwi.apus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ApusApplication {
@@ -10,4 +13,8 @@ public class ApusApplication {
 		SpringApplication.run(ApusApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
 }
